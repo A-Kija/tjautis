48f694cb2c8a489d29bf956e2036346c215941f9
A-Kija <arvydas.kijakauskas@gmail.com>
2022-02-14 11:07:01 +0200

    100 begemotu

--- 001/index.js
+++ 001/index.js
@@ -8,10 +8,21 @@
 
 const animal = 'Begemotas';
 
-console.log(animal[0]);
-console.log(animal[6]);
+// console.log(animal[0]);
+// console.log(animal[6]);
 
 
-for (let i = 0; i < animal.length; i++) {
-    console.log(animal[i], i);
+// for (let i = 0; i < animal.length; i++) {
+//     console.log(animal[i], i);
+// }
+
+
+
+let animal100 = '';
+
+for (let i = 0; i < 100; i++) {
+    animal100 = animal100 + ' ' + animal;
+    // animal100 += animal; tas pats
 }
+
+console.log(animal100);
