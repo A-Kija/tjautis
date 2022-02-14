2dae31ee4cadc78d4933aacc5219adcc82e54c5f
A-Kija <arvydas.kijakauskas@gmail.com>
2022-02-14 10:53:44 +0200

    start

---
+++ 001/index.js
@@ -0,0 +1,17 @@
+// for (let i = 10; i > 0; i--) {
+//     console.log(i);
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(10 - i);
+// }
+
+const animal = 'Begemotas';
+
+console.log(animal[0]);
+console.log(animal[6]);
+
+
+for (let i = 0; i < animal.length; i++) {
+    console.log(animal[i], i);
+}
